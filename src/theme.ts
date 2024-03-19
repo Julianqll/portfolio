84c321e8651ccbe73fe0148e11d427a28151678f
Julian Quispe <julianmql@hotmail.com>
2024-03-19 02:11:10 -0500

    fix: background stuff fixed

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,10 @@
 import { createTheme } from "@mantine/core";
+import { generateColors } from '@mantine/colors-generator';
 
 export const theme = createTheme({
-
+    colors: {
+        'julian-blue': generateColors('#11284b'),
+      },
+      primaryColor: 'julian-blue',
+      autoContrast: true,
 });
